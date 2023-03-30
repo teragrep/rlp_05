af549afe265e7908ff8a9c035e3d978620ba1b5b
eemhu <125959687+eemhu@users.noreply.github.com>
2023-03-30 13:53:50 +0300

    RelpConnection_test launches relp server; add disconnect test

--- src/main/go/RelpConnection_test.go
+++ src/main/go/RelpConnection_test.go
@@ -2,14 +2,24 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"os"
+	"os/exec"
+	"path/filepath"
 	"strconv"
+	"strings"
 	"testing"
+	"time"
 )
 
 // TestConnection: Sends OPEN->SYSLOG->CLOSE messages.
 // Checks for window (pending) to be empty and also that the batch's workQueue is empty.
 // Meaning all pending operations have been verified
 func TestConnection(t *testing.T) {
+	relpServer := InitServerConnection()
+	time.Sleep(time.Second)
+	// server ok, actual test
+
 	sess := RelpConnection{}
 	sess.Init()
 	ok, _ := sess.Connect("127.0.0.1", 1601)
@@ -26,7 +36,10 @@ func TestConnection(t *testing.T) {
 		data:       []byte("HelloThisIsAMessage"),
 	}})
 
-	sess.Commit(&msgBatch)
+	err := sess.Commit(&msgBatch)
+	if err != nil {
+		t.Errorf("Error committing batch (err!=nil); want nil")
+	}
 	disOk := sess.Disconnect()
 
 	if !disOk {
@@ -42,12 +55,21 @@ func TestConnection(t *testing.T) {
 	if msgBatch.GetWorkQueueLen() != 0 {
 		t.Errorf("RelpBatch.WorkQueue was not empty! (len=%v); want 0", msgBatch.GetWorkQueueLen())
 	}
+
+	// kill server
+	err = relpServer.Process.Kill()
+	if err != nil {
+		t.Error("Could not kill server\n")
+	}
 }
 
 // TestConnectionMulti: Sends OPEN->SYSLOG->SYSLOG->SYSLOG->CLOSE messages.
 // Checks for window (pending) to be empty and also that the batch's workQueue is empty.
 // Meaning all pending operations have been verified
 func TestConnectionMulti(t *testing.T) {
+	relpServer := InitServerConnection()
+	time.Sleep(time.Second)
+	// server ok, actual test
 	sess := RelpConnection{}
 	sess.Init()
 	ok, _ := sess.Connect("127.0.0.1", 1601)
@@ -67,7 +89,10 @@ func TestConnectionMulti(t *testing.T) {
 			data:       syslogMsg,
 		}})
 
-		sess.Commit(&msgBatch)
+		err := sess.Commit(&msgBatch)
+		if err != nil {
+			t.Errorf("Error committing batch (err!=nil); want nil")
+		}
 
 		// batch queue empty
 		if msgBatch.GetWorkQueueLen() != 0 {
@@ -85,12 +110,21 @@ func TestConnectionMulti(t *testing.T) {
 	if sess.window.Size() != 0 {
 		t.Errorf("RelpConnection.Window was not empty! (size=%v); want 0", sess.window.Size())
 	}
+
+	// kill server
+	err := relpServer.Process.Kill()
+	if err != nil {
+		t.Error("Could not kill server\n")
+	}
 }
 
 // TestConnectionMulti: Sends OPEN->SYSLOG(m.)->SYSLOG->SYSLOG(m.)->CLOSE messages.
 // Checks for window (pending) to be empty and also that the batch's workQueue is empty.
 // Meaning all pending operations have been verified
 func TestConnectionMultiBatch(t *testing.T) {
+	relpServer := InitServerConnection()
+	time.Sleep(time.Second)
+	// server ok, actual test
 	sess := RelpConnection{}
 	sess.Init()
 	ok, _ := sess.Connect("127.0.0.1", 1601)
@@ -129,7 +163,10 @@ func TestConnectionMultiBatch(t *testing.T) {
 
 		fmt.Printf("Got workQ: %v\n", msgBatch.GetWorkQueueLen())
 
-		sess.Commit(&msgBatch)
+		err := sess.Commit(&msgBatch)
+		if err != nil {
+			t.Errorf("Error committing batch (err!=nil); want nil")
+		}
 
 		// batch queue empty
 		if msgBatch.GetWorkQueueLen() != 0 {
@@ -147,4 +184,141 @@ func TestConnectionMultiBatch(t *testing.T) {
 	if sess.window.Size() != 0 {
 		t.Errorf("RelpConnection.Window was not empty! (size=%v); want 0", sess.window.Size())
 	}
+
+	// kill server
+	err := relpServer.Process.Kill()
+	if err != nil {
+		t.Error("Could not kill server\n")
+	}
+}
+
+// TestConnectionHandleDisconnect: Sends OPEN->SYSLOG(m.)->SYSLOG->SYSLOG(m.)->CLOSE messages,
+// with a server disconnect in between.
+// Checks for window (pending) to be empty and also that the batch's workQueue is empty.
+// Meaning all pending operations have been verified
+func TestConnectionHandleDisconnect(t *testing.T) {
+	relpServer := InitServerConnection()
+	time.Sleep(time.Second)
+
+	// server ok, actual test
+	sess := RelpConnection{}
+	sess.Init()
+	retryRelpConnection(&sess)
+
+	for i := 0; i < 3; i++ {
+		syslogMsg := []byte("HelloThisIsAMessage" + strconv.FormatInt(int64(i), 10))
+		syslogMsgLen := len(syslogMsg)
+		msgBatch := RelpBatch{}
+		msgBatch.Init()
+		msgBatch.PutRequest(&RelpFrameTX{RelpFrame{
+			cmd:        RELP_SYSLOG,
+			dataLength: syslogMsgLen,
+			data:       syslogMsg,
+		}})
+
+		// kill server after first batch for 2 seconds
+		if i == 1 {
+			go func() {
+				err := relpServer.Process.Kill()
+				if err != nil {
+					t.Errorf("Could not kill server\n")
+				}
+				time.Sleep(2 * time.Second)
+				relpServer = InitServerConnection()
+			}()
+		}
+
+		// put 3 messages on batches 0 and 2, and 1 message on batch 1
+		if i != 1 {
+			msgBatch.PutRequest(&RelpFrameTX{RelpFrame{
+				cmd:        RELP_SYSLOG,
+				dataLength: syslogMsgLen,
+				data:       syslogMsg,
+			}})
+			msgBatch.PutRequest(&RelpFrameTX{RelpFrame{
+				cmd:        RELP_SYSLOG,
+				dataLength: syslogMsgLen,
+				data:       syslogMsg,
+			}})
+		}
+
+		notDone := true
+		for notDone {
+			commitErr := sess.Commit(&msgBatch)
+			if commitErr != nil {
+				log.Printf("Error committing batch: '%v'\n", commitErr.Error())
+			}
+
+			if !msgBatch.VerifyTransactionAll() {
+				msgBatch.RetryAllFailed()
+				retryRelpConnection(&sess)
+			} else {
+				notDone = false
+			}
+		}
+
+		// batch queue empty
+		if msgBatch.GetWorkQueueLen() != 0 {
+			t.Errorf("RelpBatch.WorkQueue was not empty! (len=%v); want 0", msgBatch.GetWorkQueueLen())
+		}
+	}
+
+	disOk := sess.Disconnect()
+
+	if !disOk {
+		t.Errorf("Disconnection was not successful! (success=%v); want true", disOk)
+	}
+
+	// no stuff pending
+	if sess.window.Size() != 0 {
+		t.Errorf("RelpConnection.Window was not empty! (size=%v); want 0", sess.window.Size())
+	}
+
+	// kill server
+	err2 := relpServer.Process.Kill()
+	if err2 != nil {
+		t.Errorf("Could not kill server\n")
+	}
+	fmt.Println("done")
+}
+
+// utils
+func retryRelpConnection(relpSess *RelpConnection) {
+	relpSess.TearDown()
+	var cSuccess bool
+	var cErr error
+	cSuccess, cErr = relpSess.Connect("127.0.0.1", 1601)
+	for !cSuccess || cErr != nil {
+		relpSess.TearDown()
+		time.Sleep(5 * time.Second)
+		cSuccess, cErr = relpSess.Connect("127.0.0.1", 1601)
+	}
+}
+
+func InitServerConnection() *exec.Cmd {
+	ready := make(chan int, 1)
+
+	// get relp server jar
+	wd, _ := os.Getwd()
+	par := filepath.Dir(wd)
+	for !strings.HasSuffix(par, "/src") {
+		par = filepath.Dir(par)
+	}
+	jarLocation := par + "/resources/relp-server/java-relp-server-demo-jar-with-dependencies.jar"
+	log.Printf("get relp server jar %v\n", jarLocation)
+
+	// run it
+	relpServer := exec.Command("/usr/bin/java",
+		"-jar", jarLocation)
+
+	// start in coroutine
+	go func() {
+		srvErr := relpServer.Start()
+		if srvErr != nil {
+			panic("failed to start relp server")
+		}
+		ready <- 1
+	}()
+	<-ready
+	return relpServer
 }
